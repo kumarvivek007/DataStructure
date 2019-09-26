5c13bdefa57941aceeda3c9dba47e9feb1ee8155
kumarvivek <kumar.vivek@sap.com>
2019-09-26 14:50:08 +0530

    adding code

--- personalWS/HR/src/SolutionAnargam.java
+++ personalWS/HR/src/SolutionAnargam.java
@@ -1,2 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class SolutionAnargam {
+
+    // Complete the makeAnagram function below.
+    static int makeAnagram(String a, String b) {
+        String
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String a = scanner.nextLine();
+
+        String b = scanner.nextLine();
+
+        int res = makeAnagram(a, b);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
