70eff43d1dbd6abac4703dd11766c680e25a229a
kumarvivek <kumar.vivek@sap.com>
2019-12-22 00:26:30 +0530

    checking in practice problem

--- personalWS/HR/src/com/hackerrank/CountVallyProblem.java
+++ personalWS/HR/src/com/hackerrank/CountVallyProblem.java
@@ -24,7 +24,7 @@ public class CountVallyProblem {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException  {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = scanner.nextInt();
